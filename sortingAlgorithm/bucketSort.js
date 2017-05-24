9ecfd8160f32dc6dfa01cf4ba7749cef6c542278
JiayiLi <jiayi_li10@163.com>
2017-05-24 20:52:26 +0800

    add bucketSort

--- sortingAlgorithm/bucketSort.js
+++ sortingAlgorithm/bucketSort.js
@@ -1,21 +1,3 @@
-var  insertionSort = function(target_array){
-	for(var i=1;i<target_array.length;i++){
-		for(var j=0;j<i;j++){
-			if(target_array[j]>target_array[i]){
-				// 将target_array[i]放入对应位置，再删除
-
-				//在j的位置加target_array[i]； 
-				//例子： var test = [1,2,4]; test.splice(2,0,3);输出 [1,2,3,4]
-				target_array.splice(j,0,target_array[i]); 
-				target_array.splice(i+1,1);
-			}
-		}
-	}
-
-	return target_array;
-}
-
-
 var bucketSort = function(target_array){
 	var min = target_array[0];
 	var max = target_array[0];
